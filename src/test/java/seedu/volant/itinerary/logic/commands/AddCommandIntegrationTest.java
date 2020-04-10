0b58259c2666a735f61cc353645e836a5f51425a
vuhieunguyen <hieunguyen98pro@gmail.com>
2020-04-11 04:48:48 +0800

    Fixed test to pass Travis

--- src/test/java/seedu/volant/itinerary/logic/commands/AddCommandIntegrationTest.java
+++ src/test/java/seedu/volant/itinerary/logic/commands/AddCommandIntegrationTest.java
@@ -2,7 +2,7 @@ package seedu.volant.itinerary.logic.commands;
 
 import static seedu.volant.itinerary.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.volant.itinerary.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.volant.testutil.TypicalActivities.getTypicalActivityList;
+import static seedu.volant.testutil.TypicalActivities.getTypicalActivities;
 import static seedu.volant.testutil.TypicalTrips.getGermanyTrip;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -13,7 +13,6 @@ import seedu.volant.itinerary.model.ItineraryModelManager;
 import seedu.volant.itinerary.model.activity.Activity;
 import seedu.volant.testutil.ActivityBuilder;
 
-
 public class AddCommandIntegrationTest {
 
     private ItineraryModelManager model;
@@ -21,7 +20,7 @@ public class AddCommandIntegrationTest {
     @BeforeEach
     public void setUp() {
         model = new ItineraryModelManager(getGermanyTrip(), new UserPrefs());
-        model.setActivityList(getTypicalActivityList());
+        model.getTrip().getItinerary().getActivityList().setActivities(getTypicalActivities());
     }
 
     @Test
@@ -31,13 +30,15 @@ public class AddCommandIntegrationTest {
         ItineraryModelManager expectedModel = new ItineraryModelManager(model.getTrip(), new UserPrefs());
 
         expectedModel.addActivity(validActivity);
+        model = new ItineraryModelManager(getGermanyTrip(), new UserPrefs());
+        model.getTrip().getItinerary().getActivityList().setActivities(getTypicalActivities());
 
         assertCommandSuccess(new AddCommand(validActivity), model,
                 String.format(AddCommand.MESSAGE_SUCCESS, validActivity), expectedModel);
     }
 
     @Test
-    public void execute_duplicateTrip_throwsCommandException() {
+    public void execute_duplicateActivity_throwsCommandException() {
         Activity activityInList = model.getActivityList().getActivityList().get(0);
         assertCommandFailure(new AddCommand(activityInList), model, AddCommand.MESSAGE_DUPLICATE_ITEM);
     }
