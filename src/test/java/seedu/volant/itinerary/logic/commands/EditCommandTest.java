1182b55866379039861ea71e9748f83d38ecc4ce
vuhieunguyen <hieunguyen98pro@gmail.com>
2020-04-11 17:30:37 +0800

    Fixed code to pass Travis

--- src/test/java/seedu/volant/itinerary/logic/commands/EditCommandTest.java
+++ src/test/java/seedu/volant/itinerary/logic/commands/EditCommandTest.java
@@ -36,7 +36,6 @@ public class EditCommandTest {
         ItineraryModelManager expectedModel = new ItineraryModelManager(model.getTrip(), new UserPrefs());
 
         expectedModel.setActivity(model.getFilteredActivityList().get(0), editedActivity);
-        
         model = new ItineraryModelManager(getGermanyTrip(), new UserPrefs());
         model.getTrip().getItinerary().getActivityList().setActivities(getTypicalActivities());
 
